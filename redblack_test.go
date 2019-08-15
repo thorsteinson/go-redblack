30996f26f5643064265d3b88189cae6879300b9b
Caleb Thorsteinson <caleb@thorsteinson.io>
2019-08-15 10:18:43 -0700

    Adds rotation inversion test
    When left and right rotatation are called successively they should
    rotate the nodes such that the tree isn't modified at all. This also
    comes with a few extra functions to support this testing.

--- redblack_test.go
+++ redblack_test.go
@@ -22,6 +22,82 @@ func TestTraversal(t *testing.T) {
 	}
 }
 
+// genTestTree is a helper function that generates a balanced size 15
+// binary tree for testing various cases, The tree is generated with
+// keys in order for easily working with the traverse function
+//
+// The in order traversal of this tree results in [n1..n15]
+//
+// This also returns a list of pointers to non leaf nodes, and leaf
+// nodes so we test out rotation and panic behaviour
+func genTestTree() (t *tree, nonLeaves []*node, leaves []*node) {
+	n1 := node{key: 1}
+	n2 := node{key: 2}
+	n3 := node{key: 3}
+	n4 := node{key: 4}
+	n5 := node{key: 5}
+	n6 := node{key: 6}
+	n7 := node{key: 7}
+	n8 := node{key: 8}
+	n9 := node{key: 9}
+	n10 := node{key: 10}
+	n11 := node{key: 11}
+	n12 := node{key: 12}
+	n13 := node{key: 13}
+	n14 := node{key: 14}
+	n15 := node{key: 15}
+
+	t = &tree{root: &n8}
+	n8.left = &n4
+	n8.right = &n14
+	n4.left = &n2
+	n4.right = &n6
+	n2.left = &n1
+	n2.right = &n3
+	n6.left = &n5
+	n6.right = &n7
+	n12.left = &n10
+	n12.right = &n14
+	n10.left = &n9
+	n10.right = &n11
+	n14.left = &n13
+	n14.right = &n15
+
+	return t, []*node{&n8, &n4, &n12, &n2, &n6, &n10, &n14}, []*node{&n1, &n3, &n5, &n7, &n9, &n11, &n13, &n15}
+}
+
+func deepEqualTree(t1 *tree, t2 *tree) bool {
+	return deepEqualHelper(t1.root, t2.root)
+}
+
+func deepEqualHelper(n1 *node, n2 *node) bool {
+	if n1 == nil && n2 == nil {
+		return true
+	} else if n1.key != n2.key {
+		return false
+	}
+	return deepEqualHelper(n1.left, n2.left) &&
+		deepEqualHelper(n1.right, n2.right)
+}
+
+// The functions for rotation should be invertible, so calling left
+// and right successfully should modify the structure such that it was
+// never modified at all
+func TestRotationInversion(t *testing.T) {
+	tree, nonLeaves, _ := genTestTree()
+
+	for _, node := range nonLeaves {
+		child := node.right
+
+		tree.rotateLeft(node)
+		tree.rotateRight(child)
+
+		if newTree, _, _ := genTestTree(); !deepEqualTree(tree, newTree) {
+			t.Errorf("Node with key %v broke inversion", node.key)
+		}
+	}
+}
+
 func TestLeftRotation(t *testing.T) {
 	n1 := node{}
 	n2 := node{}
