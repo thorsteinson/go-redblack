26ff5dc901ad376771e88fc83912316d846f0f80
Caleb Thorsteinson <caleb@thorsteinson.io>
2019-08-14 19:13:46 -0700

    Adds initial tests for rotation functions
    These aren't substantive, but they are a good start. These only test a
    single case by building up a tree.

--- redblack_test.go
+++ redblack_test.go
@@ -21,3 +21,37 @@ func TestTraversal(t *testing.T) {
 		t.Error("Improper ordering found in traversal")
 	}
 }
+
+func TestLeftRotation(t *testing.T) {
+	n1 := node{}
+	n2 := node{}
+	n3 := node{}
+	n4 := node{}
+
+	tr := &tree{root: &n1}
+	n1.right = &n2
+	n2.left = &n3
+	n2.right = &n4
+
+	expectedOrder := []*node{&n1, &n3, &n2, &n4}
+	if !reflect.DeepEqual(expectedOrder, tr.traverse()) {
+		t.Error("Improper left rotation")
+	}
+}
+
+func TestRightRotation(t *testing.T) {
+	n1 := node{}
+	n2 := node{}
+	n3 := node{}
+	n4 := node{}
+
+	tr := &tree{root: &n1}
+	n1.right = &n2
+	n2.left = &n3
+	n2.right = &n4
+
+	expectedOrder := []*node{&n1, &n3, &n2, &n4}
+	if !reflect.DeepEqual(expectedOrder, tr.traverse()) {
+		t.Error("Improper right rotation")
+	}
+}
